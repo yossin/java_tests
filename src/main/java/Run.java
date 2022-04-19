337475959c882700c1d894324d3c57b5c3d85638
yos <yos@yosmail.com>
2022-04-19 19:51:40 +0000

    initial project

---
+++ src/main/java/Run.java
@@ -0,0 +1,5 @@
+public class Run {
+ public static void main(byte[] args) {
+     System.out.println("hello");
+ }   
+}
---
+++ src/test/java/RunTests.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class RunTests {
+    @Test
+    public void test1(){
+        assertTrue(true);
+    }
+}
